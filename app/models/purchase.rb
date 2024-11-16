b908fd9c77cd72b1049a35d96cc022e2a3b2c369
roberto prada <ing.robertoprada@gmail.com>
2024-11-16 12:11:15 -0300

    validations complete

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,15 @@ class Purchase < ApplicationRecord
   belongs_to :supplier
   has_many :purchase_details
   has_many :products, through: :purchase_details
+
+  validates :order_date, presence: true
+  validates :total_price, numericality: { greater_than: 0 }
+  validate :expected_delivery_date_after_order_date, presence: true
+end
+
+def expected_delivery_date_after_order_date
+  return unless expected_delivery_date && order_date
+  if expected_delivery_date < order_date
+    errors.add(:expected_delivery_date, "debe ser posterior a la fecha de pedido")
+  end
 end
--- app/models/purchase_detail.rb
+++ app/models/purchase_detail.rb
@@ -1,4 +1,7 @@
 class PurchaseDetail < ApplicationRecord
   belongs_to :purchase
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
 end
