fe5af575421e0958b6706c0ebd21aad69f6909b7
Valentin Cadel <valencadel@gmail.com>
2024-11-30 11:08:49 -0300

    migration changes added to company and purchases

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,7 @@ class Company < ApplicationRecord
   has_many :products
   has_many :employees
   has_many :suppliers
+  has_many :purchases
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :name, length: { maximum: 100 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_23_200146) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_30_140322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,7 +116,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_23_200146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "supplier_id", null: false
-    t.bigint "company_id"
+    t.bigint "company_id", null: false
     t.index ["company_id"], name: "index_purchases_on_company_id"
     t.index ["supplier_id"], name: "index_purchases_on_supplier_id"
   end
