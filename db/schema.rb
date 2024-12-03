ef01c655e60ebe1a310da78b5845e45355b1f949
gonzalesomar <gonzales.omar@pucp.edu.pe>
2024-12-03 18:41:53 -0500

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_03_001946) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "assets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "company_id", null: false
-    t.index ["company_id"], name: "index_assets_on_company_id"
-  end
-
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -189,7 +182,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_03_001946) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "assets", "companies"
   add_foreign_key "companies", "users"
   add_foreign_key "customers", "companies"
   add_foreign_key "products", "companies"
